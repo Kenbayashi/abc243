349bb650f4cbd0b58ff64dbc02ca4677d17ad888
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-14 08:59:52 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,31 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        mut v: u64,
+        a: u64,
+        b: u64,
+        c: u64,
+    };
+
+    loop {
+        if a <= v {
+            v -= a;
+        } else {
+            println!("F");
+            break;
+        }
+
+        if b <= v {
+            v -= b;
+        } else {
+            println!("M");
+            break;
+        }
+
+        if c <= v {
+            v -= c;
+        } else {
+            println!("T");
+            break;
+        }
+    }
 }
