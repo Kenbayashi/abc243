7aa4c35085cc3cfe4bad42da6a32614865f51e8c
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-14 10:12:44 +0900

    d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        x: u64,
+        s: Chars,
+    }
+
+    let ans = s.into_iter()
+               .fold(x, |acc, c| {
+                    match c {
+                        'L' => 2 * acc,
+                        'R' => 2 * acc + 1,
+                        'U' => acc / 2,
+                         _  => acc,
+                    }
+                });
+
+    println!("{}", ans);
 }
